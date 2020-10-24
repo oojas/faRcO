9fc1e25c6541003b245c12a8d0f04b4d851acee9
oojas <oojasgupta@gmail.com>
2020-10-24 08:14:44 +0530

    Completed the screen

--- lib/Screens/Map.dart
+++ lib/Screens/Map.dart
@@ -6,41 +6,104 @@ class UI extends StatelessWidget {
     final width = MediaQuery.of(context).size.height;
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        elevation: 25,
+        selectedItemColor: Colors.purple,
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
           BottomNavigationBarItem(
-              icon: Icon(Icons.dashboard), title: Text('Dashboard')),
+              icon: Icon(Icons.home),
+              label: 'Home',
+              backgroundColor: Colors.black87),
           BottomNavigationBarItem(
-              icon: Icon(Icons.attractions), title: Text('Attractions')),
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+            backgroundColor: Colors.purple.shade500,
+          ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.person), title: Text('Profile')),
+            icon: Icon(Icons.attractions),
+            label: 'Attractions',
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+              ),
+              label: 'Person'),
         ],
-        backgroundColor: Colors.purple.shade200,
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Container(
             decoration: BoxDecoration(
                 gradient: LinearGradient(
                   colors: [
-                    Colors.amber,
-                    Colors.amberAccent,
-                    Colors.orangeAccent,
+                    Colors.purple.shade200,
+                    Colors.pink.shade200,
+                    Colors.purple.shade300,
+                    Colors.pink.shade300,
                   ],
                 ),
                 borderRadius: BorderRadius.only(
                     bottomLeft: Radius.circular(12),
                     bottomRight: Radius.circular(100))),
-            height: width / 2,
+            height: width / 3,
+          ),
+          SizedBox(
+            height: 80,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Container(
-                width: 40,
-                height: 20,
-                decoration: BoxDecoration(),
+                width: 155,
+                height: 170,
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12),
+                    gradient: LinearGradient(colors: [
+                      Colors.purple.shade100,
+                      Colors.pink.shade100
+                    ])),
+              ),
+              Container(
+                width: 155,
+                height: 170,
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12),
+                    gradient: LinearGradient(colors: [
+                      Colors.purple.shade100,
+                      Colors.pink.shade100
+                    ])),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 155,
+                height: 170,
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12),
+                    gradient: LinearGradient(colors: [
+                      Colors.purple.shade100,
+                      Colors.pink.shade100
+                    ])),
+              ),
+              Container(
+                width: 155,
+                height: 170,
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12),
+                    gradient: LinearGradient(colors: [
+                      Colors.purple.shade100,
+                      Colors.pink.shade100
+                    ])),
               )
             ],
           ),
